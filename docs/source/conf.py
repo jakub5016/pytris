0a3511c8593b4acace2c2a09a3aab0cddb956beb
jakub5016 <me.jakub.gil@gmail.com>
2023-09-23 17:28:53 +0200

    Add theme to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ author = 'jakub5016'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
