16a0fe34add4bbb21e5d04a35e01e9357c2d62ef
jakub5016 <me.jakub.gil@gmail.com>
2023-09-25 15:37:36 +0200

    Simplyfy position by creating x and y args

--- game/main.py
+++ game/main.py
@@ -13,19 +13,29 @@ TETROMINO = {"I": [[0,0,0,0],[1,1,1,1]],
 class Block():
     """
     Definition for blocks used in game. All of the blocks are represented by Tetromino.
-    :param type: this variable is used to  
+    
+    type - this variable is used to define type of the block used
     """
     def __init__(self, type="I"):
         self._type = type
 
+        # We are using the smallest type of representation as we can, so in "I" type of block
+        # we simply use only one dimensional array.
+        # Also we want to shrik the array for 2x3 type of blocks. 
         if type != "I":
             self.representation = [np.array(TETROMINO[type][0][0:3]), np.array(TETROMINO[type][1][0:3])]
-            self.position = [0,3]
 
         else:
             self.representation = [np.array(TETROMINO[type][1])]
-            self.position = [0,3]
 
+
+        self.position = [0,3]
+
+        self.x=self.position[0]
+        self.y=self.position[1]
+
+    def update_position(self):
+        self.position = [self.x, self.y]
 
 class Board():
     def __init__(self):
@@ -37,18 +47,18 @@ class Board():
         block_arr = block.representation
 
         if block._type != "I":
-            self._status[block.position[0]][block.position[1]:block.position[1]+3] = block_arr[0]
-            self._status[block.position[0] + 1][block.position[1]:block.position[1]+3] = block_arr[1]
+            self._status[block.x][block.y:block.y+3] = block_arr[0]
+            self._status[block.x + 1][block.y:block.y+3] = block_arr[1]
 
         else:
 
-            self._status[block.position[0]][block.position[1]:7] = block_arr[0]
+            self._status[block.x][block.y:7] = block_arr[0]
 
 
     def move_block_down(self):
-        if (self.elements[-1].position[0] + 1) <= (len(self._status) - len(self.elements[-1].position)): # Assertion from hitting floor
+        if (self.elements[-1].x + 1) <= (len(self._status) - len(self.elements[-1].position)): # Assertion from hitting floor
             self.clear_elem()
-            self.elements[-1].position[0] += 1
+            self.elements[-1].x += 1
             self.refresh_position()
 
             is_touched=False
@@ -57,7 +67,7 @@ class Board():
                     if j >= 2:
                         is_touched=True
                         self.clear_elem()
-                        self.elements[-1].position[0] -= 1
+                        self.elements[-1].x -= 1
                         self.refresh_position()
 
             if is_touched:
@@ -75,12 +85,12 @@ class Board():
             block = self.elements[-1]
 
         if block._type != "I":
-            self._status[block.position[0]][block.position[1]:block.position[1]+3] = 0
-            self._status[block.position[0] + 1][block.position[1]:block.position[1]+3] = 0
+            self._status[block.x][block.y:block.y+3] = 0
+            self._status[block.x + 1][block.y:block.y+3] = 0
 
         else:
 
-             self._status[block.position[0]][block.position[1]:block.position[1]+4] = 0
+             self._status[block.x][block.y:block.y+4] = 0
 
 
     def refresh_position(self, block=None, addition=True):
@@ -90,32 +100,32 @@ class Board():
 
         if addition:
             if block._type != "I":
-                self._status[block.position[0]][block.position[1]:block.position[1]+3] += block_arr[0]
-                self._status[block.position[0] + 1][block.position[1]:block.position[1]+3] += block_arr[1]
+                self._status[block.x][block.y:block.y+3] += block_arr[0]
+                self._status[block.x + 1][block.y:block.y+3] += block_arr[1]
 
             else:
-                print(self._status[block.position[0]][block.position[1]:block.position[1]+4], block.position[0])
-
-                self._status[block.position[0]][block.position[1]:block.position[1]+4] += block_arr[0]
+                self._status[block.x][block.y:block.y+4] += block_arr[0]
         else:
             if block._type != "I":
-                self._status[block.position[0]][block.position[1]:block.position[1]+3] = block_arr[0]
-                self._status[block.position[0] + 1][block.position[1]:block.position[1]+3] = block_arr[1]
+                self._status[block.x][block.y:block.y+3] = block_arr[0]
+                self._status[block.x + 1][block.y:block.y+3] = block_arr[1]
 
             else:
 
-                self._status[block.position[0]][block.position[1]:block.position[1]+4] = block_arr[0]
+                self._status[block.x][block.y:block.y+4] = block_arr[0]
+
+        block.update_position()        
 
     def move_left(self):
         self.clear_elem()
-        if (self.elements[-1].position[1] - 1) >= 0:  # Assertion from hitting wall
-            self.elements[-1].position[1] -= 1
+        if (self.elements[-1].y - 1) >= 0:  # Assertion from hitting wall
+            self.elements[-1].y -= 1
             self.refresh_position()
         
     def move_right(self):
         self.clear_elem()
-        if (self.elements[-1].position[1] + 1) <= (len(self._status[0]) - len(self.elements[-1].representation[0])):  # Assertion from hitting wall
-            self.elements[-1].position[1] += 1
+        if (self.elements[-1].y + 1) <= (len(self._status[0]) - len(self.elements[-1].representation[0])):  # Assertion from hitting wall
+            self.elements[-1].y += 1
             self.refresh_position()
 
 if __name__ == "__main__":
